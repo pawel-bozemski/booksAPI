85a6c35852f45506a8dd1bfb916749bf1990fc5f
Pawel Bozemski <paw.boz@gmail.com>
2020-04-06 14:06:58 +0200

    Finish tests

---
+++ utils/formatFullname.js
@@ -0,0 +1,8 @@
+module.exports = (fullName) => {
+  if (typeof fullName !== 'string') return 'Error';
+  const splittedFullName = fullName.split(' ');
+
+  const [ firstName, lastName ] = splittedFullName;
+  if(!firstName || !lastName || splittedFullName.length >2 ) return 'Error';
+  return firstName[0].toUpperCase() + firstName.slice(1).toLowerCase() + ' ' + lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
+};
